9535be1a02472dc118ad3205dd6154f0b03b0ae6
ghouse2004 <shaikg135@gmail.com>
2024-12-05 12:47:26 +0530

    changed js file

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    alert('Welcome to the Team Portfolio!');
-  });
+document.querySelectorAll('.profile').forEach(profile => {
+        profile.addEventListener('mouseenter', () => {
+          profile.style.backgroundColor = '#f0f0f0';
+        });
+        profile.addEventListener('mouseleave', () => {
+          profile.style.backgroundColor = 'white';
+        });
+      });
+    
